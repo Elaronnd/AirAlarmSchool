abb17ea9717476fda4e2b25a537d652d82413d0b
Elaronnd <elaronnd@gmail.com>
2024-04-03 21:30:32 +0300

    added cooldown button

--- web/index.js
+++ web/index.js
@@ -1,4 +1,5 @@
-async function getDataFromPythonForce() {         document.getElementById("myele").innerText = await eel.get_data_force()();
+async function getDataFromPythonForce() {
+    document.getElementById("myele").innerText = await eel.get_data_force()();
     document.getElementById("date").innerText = await eel.get_date_time()();
     if (await eel.get_data_force()() === "[info] На даний момент в Києві повітряна тривога") {
       var li = document.getElementById("myele");
@@ -13,5 +14,14 @@ document.getElementById("mybtn").addEventListener("click", async()=> {
     getDataFromPythonForce();
 })
 
+function startCooldown() {
+  document.getElementById("mybtn").disabled = true;
+
+  setTimeout(function () {
+    document.getElementById("mybtn").disabled = false;
+  }, 5000);
+}
+
+document.getElementById("mybtn").addEventListener("click", startCooldown);
 getDataFromPythonForce();
 setInterval(getDataFromPythonForce, 5000);
