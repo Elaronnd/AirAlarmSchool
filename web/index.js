fde90530be5ea9fab583d85e07cf77456812cbce
denys <lifenko.denis2790@gmail.com>
2024-04-04 21:19:32 +0300

    changed id`s

--- web/index.js
+++ web/index.js
@@ -1,27 +1,27 @@
 async function getDataFromPythonForce() {
-    document.getElementById("myele").innerText = await eel.get_data_force()();
+    document.getElementById("stat").innerText = await eel.get_data_force()();
     document.getElementById("date").innerText = await eel.get_date_time()();
     if (await eel.get_data_force()() === "[info] На даний момент в Києві повітряна тривога") {
-      var li = document.getElementById("myele");
+      var li = document.getElementById("stat");
       li.style.color = "red";
 }   else {
-      var li = document.getElementById("myele");
+      var li = document.getElementById("stat");
       li.style.color = "green";
   }
 }
 
-document.getElementById("mybtn").addEventListener("click", async()=> {
+document.getElementById("button").addEventListener("click", async()=> {
     getDataFromPythonForce();
 })
 
 function startCooldown() {
-  document.getElementById("mybtn").disabled = true;
+  document.getElementById("button").disabled = true;
 
   setTimeout(function () {
-    document.getElementById("mybtn").disabled = false;
+    document.getElementById("button").disabled = false;
   }, 5000);
 }
 
-document.getElementById("mybtn").addEventListener("click", startCooldown);
+document.getElementById("button").addEventListener("click", startCooldown);
 getDataFromPythonForce();
 setInterval(getDataFromPythonForce, 5000);
