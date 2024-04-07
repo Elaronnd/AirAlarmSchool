4ceee627616d3e2b1afc37e89380b7f03eb73243
Elaronnd <elaronnd@gmail.com>
2024-04-07 11:20:53 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_data_force(place):
     api_url = 'https://vadimklimenko.com/map/statuses.json'
     response = requests.get(api_url)
     if response.status_code == requests.codes.ok:
-        if response.json()["states"][place]["enabled"] == True:
+        if response.json()["states"][place]["enabled"] is True:
             return True
         else:
             return False
@@ -23,7 +23,7 @@ def get_date_time(place):
     api_url = 'https://vadimklimenko.com/map/statuses.json'
     response = requests.get(api_url)
     if response.status_code == requests.codes.ok:
-        if response.json()["states"][place]["enabled_at"] == None:
+        if response.json()["states"][place]["enabled_at"] is None:
             time = response.json()["states"][place]["disabled_at"][11:19]
             date = response.json()["states"][place]["disabled_at"][0:10]
             if time[0:2] == ["21", "22", "23", "24"]:
@@ -37,7 +37,7 @@ def get_date_time(place):
         else:
             time = response.json()["states"][place]["enabled_at"][11:19]
             date = response.json()["states"][place]["enabled_at"][0:10]
-            if time[0:2] == ["21", "22", "23", "24"]:
+            if time[0:2] in ["21", "22", "23", "24"]:
                 date = datetime.datetime.strptime(date, "%Y-%m-%d")
                 date += datetime.timedelta(days=1)
                 date = date.strftime("%Y-%m-%d")
@@ -56,14 +56,14 @@ def lesson(place):
     api_url = 'https://vadimklimenko.com/map/statuses.json'
     response = requests.get(api_url)
     if response.status_code == requests.codes.ok:
-        if response.json()["states"][place]["enabled"] == False or response.json()["states"][place]["enabled"] == True:
+        if response.json()["states"][place]["enabled"] is False or response.json()["states"][place]["enabled"] is True:
             if datetime.time(8, 30) > current_time.time():
                 return "Уроки поки що не почалися"
             elif datetime.time(15, 5) < current_time.time():
                 return "Уроки закінчилися, йди додому"
-            elif current_week == [5, 6]:
+            elif current_week in [5, 6]:
                 return "Відпочивай зараз вихідний день!"
-            elif datetime.time(9, 16) <= current_time.time() >= datetime.time(9, 24):
+            elif datetime.time(9, 16) <= current_time.time() <= datetime.time(9, 24):
                 return f"Зараз перша маленька перерва (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(9, 16))})"
             elif datetime.time(10, 11) <= current_time.time() <= datetime.time(10, 29):
                 return f"Зараз перша велика перерва (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(10, 11))})"
@@ -78,7 +78,7 @@ def lesson(place):
 
             elif current_week == 0 and datetime.time(8, 30) <= current_time.time() <= datetime.time(9, 15):
                 return f"Зараз перший урок: Українська мова або Англійська мова (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(8, 30))})"
-            elif current_week == 0 and datetime.time(9, 25) <= current_time.time() >= datetime.time(10, 10):
+            elif current_week == 0 and datetime.time(9, 25) <= current_time.time() <= datetime.time(10, 10):
                 return f"Зараз другий урок: Українська мова або Англійська мова (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(9, 25))})"
             elif current_week == 0 and datetime.time(10, 30) <= current_time.time() <= datetime.time(11, 15):
                 return f"Зараз третій урок: Англійська мова або Українська мова (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(10, 30))}"
@@ -93,7 +93,7 @@ def lesson(place):
 
             elif current_week == 1 and datetime.time(8, 30) <= current_time.time() <= datetime.time(9, 15):
                 return f"Зараз перший урок: Українська література (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(8, 30))})"
-            elif current_week == 1 and datetime.time(9, 25) <= current_time.time() >= datetime.time(10, 10):
+            elif current_week == 1 and datetime.time(9, 25) <= current_time.time() <= datetime.time(10, 10):
                 return f"Зараз другий урок: Українська література (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(9, 25))})"
             elif current_week == 1 and datetime.time(10, 30) <= current_time.time() <= datetime.time(11, 15):
                 return f"Зараз третій урок: Фізика (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(10, 30))}"
@@ -108,7 +108,7 @@ def lesson(place):
 
             elif current_week == 2 and datetime.time(8, 30) <= current_time.time() <= datetime.time(9, 15):
                 return f"Зараз перший урок: Алгебра (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(8, 30))})"
-            elif current_week == 2 and datetime.time(9, 25) <= current_time.time() >= datetime.time(10, 10):
+            elif current_week == 2 and datetime.time(9, 25) <= current_time.time() <= datetime.time(10, 10):
                 return f"Зараз другий урок: Алгебра (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(9, 25))})"
             elif current_week == 2 and datetime.time(10, 30) <= current_time.time() <= datetime.time(11, 15):
                 return f"Зараз третій урок: Фізика (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(10, 30))}"
@@ -123,7 +123,7 @@ def lesson(place):
 
             elif current_week == 3 and datetime.time(8, 30) <= current_time.time() <= datetime.time(9, 15):
                 return f"Зараз перший урок: Геометрія (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(8, 30))})"
-            elif current_week == 3 and datetime.time(9, 25) <= current_time.time() >= datetime.time(10, 10):
+            elif current_week == 3 and datetime.time(9, 25) <= current_time.time() <= datetime.time(10, 10):
                 return f"Зараз другий урок: Геометрія (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(9, 25))})"
             elif current_week == 3 and datetime.time(10, 30) <= current_time.time() <= datetime.time(11, 15):
                 return f"Зараз третій урок: Англійська мова або Українська мова (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(10, 30))}"
@@ -138,7 +138,7 @@ def lesson(place):
 
             elif current_week == 4 and datetime.time(8, 30) <= current_time.time() <= datetime.time(9, 15):
                 return f"Зараз перший урок: Алгебра (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(8, 30))})"
-            elif current_week == 4 and datetime.time(9, 25) <= current_time.time() >= datetime.time(10, 10):
+            elif current_week == 4 and datetime.time(9, 25) <= current_time.time() <= datetime.time(10, 10):
                 return f"Зараз другий урок: Фізика (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(9, 25))})"
             elif current_week == 4 and datetime.time(10, 30) <= current_time.time() <= datetime.time(11, 15):
                 return f"Зараз третій урок: Зарубіжна література (Вона вже йде - {datetime.datetime.combine(datetime.date.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.date.today(), datetime.time(10, 30))}"
@@ -154,5 +154,6 @@ def lesson(place):
                 return "not pass"
         else:
             return "Неправильно вказана область/місто"
+
 
 eel.start('index.html', port=80, mode=None, shutdown_delay=float('inf'))
